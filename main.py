913ecd8ec325c396a492e2afdfd0535f53a6320c
JuanEC29 <juanestradac29@gmail.com>
2021-12-07 15:19:23 -0500

    Uptadated main 2

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ import numpy as np
 import time
 import struct
 
-serialPort = serial.Serial(port="COM4", baudrate=9600, bytesize=8, timeout=10) #Cambiar puerto serial según conexión
-f = open(f'Prueba-UHEV_{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}.csv', 'w')
-
 errors = 0
 last_tstamp = 0
 total_processed = 0
-
 
 #Estructura de los mensajes:
 
@@ -19,59 +15,58 @@ total_processed = 0
 #   time,ID,Voltage,Current, 0x13 0x10  ID=2
 
 def voltage_current(line):
-    voltaje = line.split(',')[1]
-    voltaje = voltaje*(5.0/1024)
-    corriente = line.split(',')[2]
-    corriente = ((corriente*(5.0/1024))-2.5)/0.1
-    t_stamp = line.split(',')[3]
+    voltage = line.split(',')[2]
+    voltage = (voltage*(5.0/1023))*0.1246684 # 0.1246684 = R2/(R1+R2)
+    corriente = line.split(',')[3]
+    corriente = ((corriente*(5.0/1023))-2.5)/0.1
+    t_stamp = line.split(',')[0]
     t_stamp = t_stamp/1000
-    print(f"{t_stamp},{voltaje:.2f},{corriente:.2f}")
-    f.write(f"{t_stamp},{voltaje:.2f},{corriente:.2f}")
+    print(f"{t_stamp},{voltage:.2f},{corriente:.2f}")
+    f.write(f"{t_stamp},{voltage:.2f},{corriente:.2f}")
     f.write("\n")
 
 
-#Arreglar escritura de los datos en el csv
 def rpm_motor(line):
-    Periodo_Motor = line.split(',')[1]
-    RPM_Motor = (1/Periodo_Motor)*60000
-    t_stamp = line.split(',')[2]
+    period_motor = line.split(',')[2]
+    rpm = (1/period_motor)*60000
+    t_stamp = line.split(',')[0]
     t_stamp = t_stamp/1000
-    print(f"{t_stamp},{voltaje:.2f},{corriente:.2f}")
-    f.write(f"{t_stamp},{voltaje:.2f},{corriente:.2f}")
+    print(f"{t_stamp},{rpm:.2f}")
+    f.write(f"{t_stamp},{rpm:.2f}")
     f.write("\n")
 
 
 def rpm_wheel(line):
-    Periodo_Llanta = line.split(',')[1]
-    RPM_Llanta = (1/Periodo_Llanta)*60000
-    t_stamp = line.split(',')[2]
-    t_stamp = t_stamp/1000
-    print(f"{t_stamp},{voltaje:.2f},{corriente:.2f}")
-    f.write(f"{t_stamp},{voltaje:.2f},{corriente:.2f}")
+    period_wheel = line.split(',')[2]
+    rpm = (1 / period_wheel) * 60000
+    t_stamp = line.split(',')[0]
+    t_stamp = t_stamp / 1000
+    print(f"{t_stamp},{rpm:.2f}")
+    f.write(f"{t_stamp},{rpm:.2f}")
     f.write("\n")
 
-while 1:
-    if serialPort.in_waiting > 0:
-        #En caso de tener datos en el puerto serial se lee cada linea, luego se separa por comas y se identifica el ID
-        serialString = serialPort.readline()
-        line=serialString.decode()
-        line=line.rstrip()
-        ID=line.split(",")[1]
-        last_tstamp = line.split(",")[0]
+with open(f'Prueba-UHEV_{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}.csv', 'w') as f:
+    with serial.Serial(port="COM4", baudrate=9600, bytesize=8, timeout=10) as s:
+        if s.in_waiting > 0:
+            #En caso de tener datos en el puerto serial se lee cada linea, luego se separa por comas y se identifica el ID
+            serialString = s.readline()
+            line=serialString.decode()
+            line=line.rstrip()
+            ID=line.split(",")[1]
+            last_tstamp = line.split(",")[0]
 
-        #Decodificar cada mensaje segun ID
-        if ID==0:
-            rpm_wheel(line)
-        elif ID==1:
-            rpm_wheel(line)
-        elif ID==2:
-            voltage_current(line)
-        else:
-            errors += 1
+            #Decodificar cada mensaje segun ID
+            if ID==0:
+                rpm_wheel(line)
+            elif ID==1:
+                rpm_motor(line)
+            elif ID==2:
+                voltage_current(line)
+            else:
+                errors += 1
 
-        total_processed += 1
-
-        if total_processed % 10 == 0:
-            f.flush()
-            #Arreglar bytes in_waiting
-            print(f"Procesado {total_processed} datos. Ultimo timestamp: {last_tstamp} Bytes en el buffer: {serialport.in_waiting} . Errores: {errors}")
+            total_processed += 1
+            if total_processed % 10 == 0:
+                f.flush()
+                #Arreglar bytes in_waiting
+                print(f"Procesado {total_processed} datos. Ultimo timestamp: {last_tstamp} Bytes en el buffer: {s.in_waiting} . Errores: {errors}")
