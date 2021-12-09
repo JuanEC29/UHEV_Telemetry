405559c629594cb1b7fbce7d3a67b8c2d2781f35
JuanEC29 <juanestradac29@gmail.com>
2021-12-09 16:30:29 -0500

    gitignore updated

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+
+import numpy as np
 import serial
 
 errors = 0
 last_tstamp = 0
 total_processed = 0
 
-#Estructura de los mensajes:
+
+# Estructura de los mensajes:
 
 #   time,ID,RPM_wheel, 0x13 0x10        ID=0
 #   time,ID,RPM_motor, 0x13 0x10        ID=1
@@ -13,11 +16,11 @@ total_processed = 0
 
 def voltage_current(line):
     voltage = line.split(',')[2]
-    voltage = (voltage*(5.0/1023))/0.1246684 # 0.1246684 = R2/(R1+R2)
+    voltage = (voltage * (5.0 / 1023)) / 0.1246684  # 0.1246684 = R2/(R1+R2)
     corriente = line.split(',')[3]
-    corriente = ((corriente*(5.0/1023))-2.5)/0.1
+    corriente = ((corriente * (5.0 / 1023)) - 2.5) / 0.1
     t_stamp = line.split(',')[0]
-    t_stamp = t_stamp/1000
+    t_stamp = t_stamp / 1000
     print(f"{t_stamp},{voltage:.2f},{corriente:.2f}")
     f.write(f"{t_stamp},{voltage:.2f},{corriente:.2f}")
     f.write("\n")
@@ -25,9 +28,9 @@ def voltage_current(line):
 
 def rpm_motor(line):
     period_motor = line.split(',')[2]
-    rpm = (1/period_motor)*60000
+    rpm = (1 / period_motor) * 60000
     t_stamp = line.split(',')[0]
-    t_stamp = t_stamp/1000
+    t_stamp = t_stamp / 1000
     print(f"{t_stamp},{rpm:.2f}")
     f.write(f"{t_stamp},{rpm:.2f}")
     f.write("\n")
@@ -42,23 +45,27 @@ def rpm_wheel(line):
     f.write(f"{t_stamp},{rpm:.2f}")
     f.write("\n")
 
+
 with open(f'Prueba-UHEV_{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}.csv', 'w') as f:
-    with serial.Serial(port="COM3", baudrate=9600, bytesize=8, timeout=10) as s:
+    with serial.Serial(port="COM8", baudrate=9600, bytesize=8, timeout=10) as s:
         while True:
             if s.in_waiting > 0:
-                #En caso de tener datos en el puerto serial se lee cada linea, luego se separa por comas y se identifica el ID
-                serialString = s.readline()
-                line=serialString.decode()
-                line=line.rstrip()
-                ID=line.split(",")[1]
+                # En caso de tener datos en el puerto serial se lee cada linea, luego se separa por comas y se identifica el ID
+                line = s.readline()
+
+                #line = serialString.decode()
+                lineInt = np.frombuffer(line, dtype=np.uint8)
+                #line = line.rstrip()*/-
+                np.core.defchararray.rstrip(line, chars=[0x10, 0x13])
+                ID = line.split(",")[1]
                 last_tstamp = line.split(",")[0]
 
-                #Decodificar cada mensaje segun ID
-                if ID==0:
+                # Decodificar cada mensaje segun ID
+                if ID == 0:
                     rpm_wheel(line)
-                elif ID==1:
+                elif ID == 1:
                     rpm_motor(line)
-                elif ID==2:
+                elif ID == 2:
                     voltage_current(line)
                 else:
                     errors += 1
@@ -66,5 +73,6 @@ with open(f'Prueba-UHEV_{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}.csv', 'w'
                 total_processed += 1
                 if total_processed % 10 == 0:
                     f.flush()
-                    #Arreglar bytes in_waiting
-                    print(f"Procesado {total_processed} datos. Ultimo timestamp: {last_tstamp} Bytes en el buffer: {s.in_waiting} . Errores: {errors}")
+                    # Arreglar bytes in_waiting
+                    print(
+                        f"Procesado {total_processed} datos. Ultimo timestamp: {last_tstamp} Bytes en el buffer: {s.in_waiting} . Errores: {errors}")
