4ca1007b0e85756fbbc9148a744feaf904994521
diegolop08 <diegolopezg01@hotmail.com>
2021-12-10 17:25:56 -0500

    Read lines from serial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 import numpy as np
 import serial
 
@@ -8,7 +7,7 @@ last_tstamp = 0
 total_processed = 0
 
 
-# Estructura de los mensajes:
+# Messages:
 
 #   time,ID,RPM_wheel, 0x13 0x10        ID=0
 #   time,ID,RPM_motor, 0x13 0x10        ID=1
@@ -26,53 +25,27 @@ def voltage_current(line):
     f.write("\n")
 
 
-def rpm_motor(line):
+def rpm(line):
     period_motor = line.split(',')[2]
-    rpm = (1 / period_motor) * 60000
+    _rpm = (1 / period_motor) * 60000
     t_stamp = line.split(',')[0]
     t_stamp = t_stamp / 1000
-    print(f"{t_stamp},{rpm:.2f}")
-    f.write(f"{t_stamp},{rpm:.2f}")
+    print(f"{t_stamp},{_rpm:.2f}")
+    f.write(f"{t_stamp},{_rpm:.2f}")
     f.write("\n")
 
 
-def rpm_wheel(line):
-    period_wheel = line.split(',')[2]
-    rpm = (1 / period_wheel) * 60000
-    t_stamp = line.split(',')[0]
-    t_stamp = t_stamp / 1000
-    print(f"{t_stamp},{rpm:.2f}")
-    f.write(f"{t_stamp},{rpm:.2f}")
-    f.write("\n")
-
-
-with open(f'Prueba-UHEV_{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}.csv', 'w') as f:
-    with serial.Serial(port="COM8", baudrate=9600, bytesize=8, timeout=10) as s:
-        while True:
-            if s.in_waiting > 0:
-                # En caso de tener datos en el puerto serial se lee cada linea, luego se separa por comas y se identifica el ID
-                serialString = serialPort.read(30)
-                serialInt=np.frombuffer(serialString, dtype=np.uint8)
-                if(serialInt[28]!=13 and serialInt[30]!=10):
-                    errores=errores+1
-                    print(errores)
-                    bant=0
-                    while(1):
-                        b=serialPort.read(1)
-                        if (bant==13 and b==10):
-                            break
-                        bant=b
-                else:
-                    print(serialInt)
-                    ID = serialInt[8]
-                    #time =  !!!-> hacer formula para sacar timepo de los primeros 8 bytes
-
-                # Decodificar cada mensaje segun ID
-                if ID == 0:
-                    rpm_wheel(serialInt)
-                elif ID == 1:
-                    rpm_motor(serialInt)
-                elif ID == 2:
-                    voltage_current(serialInt)
-                else:
-                    errors += 1
+serialPort = serial.Serial(port="COM4", baudrate=115200, bytesize=8, timeout=10)
+f = open(f'Prueba-UHEV_{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}.csv', 'w')
+while 1:
+    if serialPort.in_waiting > 0:
+        serialString = serialPort.readline()
+        print(serialString.decode('Ascii'))
+        #serialInt = np.frombuffer(serialString, dtype=np.uint8)
+        #print(serialInt)
+        #ID = serialInt[8]
+        # time =  !!!-> hacer formula para sacar timepo de los primeros 8 bytes
+        #if ID == 0 or ID == 1:
+        #    rpm(serialInt)
+        #elif ID == 2:
+        #    voltage_current(serialInt)
--- uC_Telemetry_test/uC_Telemetry_test.ino
+++ uC_Telemetry_test/uC_Telemetry_test.ino
@@ -2,47 +2,25 @@
 UHEV :)
 */
 
-
-#define pinLlanta 2 //Pin de interrupción de sensor hall para la Llanta
-#define pinMotor 3 //Pin de interrupción de sensor hall para el motor
+//#define pinLlanta 2 //Pin de interrupción de sensor hall para la Llanta
+//#define pinMotor 3 //Pin de interrupción de sensor hall para el motor
 #define pinVoltaje A0 //Pin del sensor de Voltaje
 #define pinCorriente A1 //Pin del sensor de Corriente
-
-volatile unsigned long t_stamp; //Timestamp
-
-volatile uint16_t voltaje = 0; //Inicializa la variable para medición de voltaje
-volatile uint16_t corriente = 0; //Inicializa la variable para medición de corriente
 
 uint16_t period_wheel = 100;
 uint16_t period_motor = 10;
 int rpm_count = 0;
 
-volatile unsigned long mseg = 0; //Contador Timestamp
+uint32_t mseg = 0;
 
-ISR(TIMER1_COMPA_vect) {
-  mseg++;
-}
 
 ISR(TIMER2_OVF_vect) {
-  t_stamp = mseg;
-  voltaje = 125; //lectura del voltaje
-  corriente = 126; //Lectura de la señal y transformacion a valor de corriente
-  SendVI(t_stamp, 2, voltaje, corriente);
+  mseg++;
+  SendVI(mseg, 2, 125, 126);
 }
 
 void setup() {
-  Serial.begin(9600); //Inicializo el serial
-
-  //Generar Interrupcion Timer 1
-  noInterrupts();
-  TCNT1 = 0;
-  TCCR1A = 0;
-  TCCR1B = 0;
-  OCR1A = 250;
-  TCCR1B |= (1 << WGM12);
-  TCCR1B |= 0b00000011; //62500 ---> 110
-  TIMSK1 |= 1 << OCIE1A;
-  interrupts();
+  Serial.begin(115200); //Inicializo el serial
 
   //Crear PIT para lectura de Voltaje y Corriente
   SREG = (SREG & 0b01111111); //Desabilitar interrupciones
@@ -53,13 +31,6 @@ void setup() {
 }
 
 void loop() {
-  rpm_count++;
-  if(rpm_count%100 == 0){
-    SendRPM(mseg,1,period_motor); //Enviar datos motor
-  }
-  if(rpm_count%1000 == 0){
-    SendRPM(mseg,0,period_wheel); //Enviar datos llanta
-  }
 }
 
 void SendRPM(unsigned long time_stamp, uint8_t id, unsigned long data_variable){
@@ -68,24 +39,34 @@ void SendRPM(unsigned long time_stamp, uint8_t id, unsigned long data_variable){
 
   time, ID, data, 0x13, 0x10
 
-  8 Bytes 1 Bytes 1 Byte  1 Bytes 8 Bytes 1 Bytes 8 Bytes 2 Bytes
-  t-stamp ,       id      ,       rpm_data ,       empty_data 0x13 0x10
+  4Bytes    1Byte    1Byte   1Byte       4Byte   1Byte    4 Bytes     2 Bytes
+  t-stamp     ,       id       ,       rpm_data    ,    empty_data   0x13 0x10
 
   ID{
   0: RPM Wheel(data1)
   1: RPM motor(data1)
   }
   */
-  Serial.write(time_stamp);
-  Serial.write(0x2C); // ,
-  Serial.write(id);
-  Serial.write(0x2C); // ,
-  Serial.write(data_variable);
-  Serial.write(0x2C); // ,
-  Serial.write(0x00);
-  Serial.write(0x00);
-  Serial.write(0x13);
-  Serial.write(0x10);
+  //Serial.write(time_stamp);
+  //Serial.write(0x2C); // ,
+  //Serial.write(id);
+  //Serial.write(0x2C); // ,
+  //Serial.write(data_variable);
+  //Serial.write(0x2C); // ,
+  //Serial.write(0x00);
+  //Serial.write(0x00);
+  //Serial.write(0x13);
+  //Serial.write(0x10);
+
+  Serial.print(time_stamp);
+  Serial.print(','); // ,
+  Serial.print(id);
+  Serial.print(','); // ,
+  Serial.print(data_variable);
+  Serial.print(','); // ,
+  Serial.print(00);
+  Serial.print(00);
+  Serial.println();
 }
 
 void SendVI(unsigned long time_stamp, uint8_t id, unsigned long voltage, unsigned long current){
@@ -94,16 +75,27 @@ void SendVI(unsigned long time_stamp, uint8_t id, unsigned long voltage, unsigne
 
   time, ID, Voltage, Current, 0x13, 0x10
 
-  8 Bytes 1 Bytes 1 Byte  1 Bytes 8 Bytes 1 Bytes 8 Bytes 2 Bytes
-  t-stamp ,       id      ,       voltage ,       current 0x13 0x10
+  4 Bytes    1Byte     1Byte   1Byte     4 Bytes   1Bytes    4Bytes    2 Bytes
+  t-stamp      ,         id      ,       voltage     ,       current  0x13 0x10
+
   */
-  Serial.write(time_stamp);
-  Serial.write(0x2C); // ,
-  Serial.write(id);
-  Serial.write(0x2C); // ,
-  Serial.write(voltage);
-  Serial.write(0x2C); // ,
-  Serial.write(current);
-  Serial.write(0x13);
-  Serial.write(0x10);
+  //Serial.write(time_stamp);
+  //Serial.write(0x2C); // ,
+  //Serial.write(id);
+  //Serial.write(0x2C); // ,
+  //Serial.write(voltage);
+  //Serial.write(0x2C); // ,
+  //Serial.write(current);
+  //Serial.write(0x13);
+  //Serial.write(0x10);
+
+
+  Serial.print(time_stamp);
+  Serial.print(','); // ,
+  Serial.print(id);
+  Serial.print(','); // ,
+  Serial.print(voltage);
+  Serial.print(','); // ,
+  Serial.print(current);
+  Serial.println();
 }
